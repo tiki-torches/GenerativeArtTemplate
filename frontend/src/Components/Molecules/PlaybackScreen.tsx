431b78e5e2011ad0b77d83ae04892796c3c59cec
Tiki Torch <atomic.tikitorch@gmail.com>
2023-02-07 11:52:38 +0900

    frontend #1 Three.jsの実行サンプルを作成

--- frontend/src/Components/Molecules/PlaybackScreen.tsx
+++ frontend/src/Components/Molecules/PlaybackScreen.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import * as THREE from 'three';
 
 /**
  * Outline	: XXXするComponent
@@ -19,25 +20,55 @@ export const PlaybackScreen : React.FC<Props> = ({ sampleProp }) => {
 
   // ___ use effect ___ ___ ___ ___ ___
   useEffect( () => { console.log(sampleState) }, [ sampleState ] );
-
+  useEffect( () => { render() }, [] );   // 描画処理はdidMount後の実行でなければ、null参照エラー（Cannot read property 'width' of null）が発生する
+  
   // ___ event handler ___ ___ ___ ___ ___
   const handleChange = (e : React.ChangeEvent<HTMLInputElement>) => {
-    const newValue = e.target.value
-    setSampleState(newValue);
   };
 
   // ___ method ___ ___ ___ ___ ___
   const test = () => {
     console.log('test');
   }
-  
+
+  const render = () => {
+    
+    const tick = () => {
+      box.rotation.y += 0.01;
+      renderer.render(scene, camera);
+      requestAnimationFrame(tick);
+    }
+
+    const width = 960;
+    const height = 540;
+
+    const renderer: any = new THREE.WebGLRenderer({
+      canvas: document.querySelector("#canvas") as HTMLCanvasElement
+    });
+    renderer.setPixelRatio(window.devicePixelRatio);
+    renderer.setSize(width, height);
+
+    const scene = new THREE.Scene();
+
+    const camera = new THREE.PerspectiveCamera(45, width / height);
+    camera.position.set(0, 0, +1000);
+
+    const geometry = new THREE.BoxGeometry(400, 400, 400);
+    const material = new THREE.MeshNormalMaterial();
+    const box = new THREE.Mesh(geometry, material);
+    scene.add(box);
+
+    tick();
+
+  };
+
   return (
     <div>
       <h2>{ PlaybackScreen.name }</h2>
-      <h3>playbackscreen</h3>
-      <h3>controls</h3>
+      <canvas id='canvas' />
     </div>
   );
+  
 };
 
 export default PlaybackScreen
