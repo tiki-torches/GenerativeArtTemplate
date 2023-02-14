689bd2719d379155e087f3fbd9a8322fc03f40a2
Tiki Torch <atomic.tikitorch@gmail.com>
2023-02-14 19:12:10 +0900

    fix #3 ReactVer18のレンダリングメソッドに変更

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import App from './Components/App';
 
-ReactDOM.render(
-    <App />, document.getElementById('app')
-);
+{/* @ts-ignore */}
+const root = createRoot(document.getElementById('app'));
+root.render(<App />);
